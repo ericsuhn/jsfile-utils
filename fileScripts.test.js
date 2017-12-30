d3ba8f7b80c1091f9d0d16d833c74ff7b44ff28e
Eric <errsuarez@gmail.com>
2017-12-30 15:55:58 -0500

    Update fileScripts.test.js

--- fileScripts.test.js
+++ fileScripts.test.js
@@ -1,18 +1,18 @@
 var assert = require('assert');
-var extract = require('./fileScripts.js');
+var fileScript = require('./fileScripts.js');
 
 test('file size of an string.txt should be 43', () => {
-  expect(extract.getFileSize('string.txt')).toBe(43);
+  expect(fileScript.getFileSize('string.txt')).toBe(43);
 });
 
 test('file name with no path of "./abc/123/hello" should be "hello" ', () => {
-  expect(extract.getFileNameWithNoPath('./abc/123/hello.txt')).toBe('hello.txt');
+  expect(fileScript.getFileNameWithNoPath('./abc/123/hello.txt')).toBe('hello.txt');
 });
 
 test('md5 of the file string.txt should match" ', () => {
-  expect(extract.getSha1('string.txt')).toBe('2fd4e1c67a2d28fced849ee1bb76e7391b93eb12');
+  expect(fileScript.getSha1('string.txt')).toBe('2fd4e1c67a2d28fced849ee1bb76e7391b93eb12');
 });
 
 test('sha1 of the file string.txt should match" ', () => {
-  expect(extract.getMd5('string.txt')).toBe('9e107d9d372bb6826bd81d3542a419d6');
+  expect(fileScript.getMd5('string.txt')).toBe('9e107d9d372bb6826bd81d3542a419d6');
 });
