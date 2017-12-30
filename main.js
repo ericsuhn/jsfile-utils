f0afef764642fbd07ea1f2e5773fa35d8551ff65
Eric <errsuarez@gmail.com>
2017-12-30 04:40:47 -0500

    Fixes#4 - output to console log

---
+++ main.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var extract = require('./fileScripts');
+var app = express();
+
+var fileSize = extract.getFileSize('string.txt');
+var fileName = extract.getFileNameWithNoPath('string.txt');
+var mdHash = extract.getMd5('string.txt');
+var shaHash = extract.getSha1('string.txt');
+
+console.log(fileName);
+console.log(fileSize);
+console.log(shaHash);
+console.log(mdHash);
